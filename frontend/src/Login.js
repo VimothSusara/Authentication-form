3a1e0e75a29aac87ebcd8ae6364a901847521fda
VimothSusara <116062952+VimothSusara@users.noreply.github.com>
2023-08-20 21:37:45 +0530

    partial login and login validation

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import validation from "./LoginValidation";
 
 function Login() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInput = (event) => {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(validation(values));
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-white vh-100">
       <div className="shadow-lg bg-light p-3 rounded-3 w-25 border border-1">
         <h2 className="text-center text-success">Sign In</h2>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email">
               <strong>Email</strong>
             </label>
             <input
               type="email"
+              name="email"
               placeholder="Email*"
               className="form-control rounded-1"
+              onChange={handleInput}
             />
           </div>
           <div className="mb-3">
@@ -23,11 +45,16 @@ function Login() {
             </label>
             <input
               type="password"
+              name="password"
               placeholder="Password*"
               className="form-control rounded-1"
+              onChange={handleInput}
             />
           </div>
-          <button className="btn btn-success w-100 mb-2 shadow-sm">
+          <button
+            type="submit"
+            className="btn btn-success w-100 mb-2 shadow-sm"
+          >
             <strong>Log In</strong>
           </button>
           <p className="text-center">Create a new account from below</p>
---
+++ frontend/src/LoginValidation.js
@@ -0,0 +1,25 @@
+function validation(values) {
+  let error = {};
+
+  const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/;
+
+  if (values.email === "") {
+    error.email = "Name is empty!";
+  } else if (!email_pattern.test(values.email)) {
+    error.email = "Email does not match";
+  } else {
+    error.email = "";
+  }
+
+  if (values.password === "") {
+    error.password = "Password is empty";
+  } else if (!password_pattern.test(values.password)) {
+    error.password = "password does not match";
+  } else {
+    error.password = "";
+  }
+  return error;
+}
+
+export default validation;
